b884af7e8f3b6b1ce122ad862f008e65ba0f1e28
node7-sh <sh@node7.org>
2024-12-13 22:34:16 +0100

    Update vib.py

--- vib.py
+++ vib.py
@@ -26,36 +26,29 @@ time.sleep(1)
 
 while True:
     start = time.time()
-#    p1 = subprocess.run(time.sleep(1))
-#    time.sleep(1)
     ts = str(int(time.time()))
     dt = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-#    os.system('clear')
+    os.system('clear')
     var0 = ("vx:{} vy:{} vz:{} ax:{} ay:{} az:{} t:{} sx:{} sy:{} sz:{} fx:{} fy:{} fz:{}".format(device0.get("58"),device0.get("59"),device0.get("60"),device0.get("61"),device0.get("62"),device0.get("63"),device0.get("64"),device0.get("65"),device0.get("66"),device0.get("67"),device0.get("68"),device0.get("69"),device0.get("70")))
     var1 = ("vx:{} vy:{} vz:{} ax:{} ay:{} az:{} t:{} sx:{} sy:{} sz:{} fx:{} fy:{} fz:{}".format(device1.get("58"),device1.get("59"),device1.get("60"),device1.get("61"),device1.get("62"),device1.get("63"),device1.get("64"),device1.get("65"),device1.get("66"),device1.get("67"),device1.get("68"),device1.get("69"),device1.get("70")))
-#    var0 = ("{},{},{}".format(device0.get("58"),device0.get("59"),device0.get("60")))
-#    var1 = ("{},{},{}".format(device1.get("58"),device1.get("59"),device1.get("60")))
     varx = dt + "," + var0 + "," + var1 + "," + ts
 #    print(varx)
     print(time.time()-next_start)
     next_start += intervall
     rest_zeit = next_start - time.time()
 
+    response = os.system("ping -c 1 " + "google.com")
+    if response == 0:
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client.connect(hostname,1883,60)
+        client.publish(topic,varx)
+        client.disconnect()
+    else:
+        pass
+    
     if rest_zeit > 0:
         time.sleep(rest_zeit)
     else:
         # Falls die Messung länger als geplant gedauert hat, wird ohne Pause weitergemacht
         # oder ggf. Fehlerbehandlung oder Logging hier einfügen.
         pass
-    
-#    response = os.system("ping -c 1 " + "google.com")
-#    if response == 0:
-#        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-#        client.connect(hostname,1883,60)
-#        client.publish(topic,varx)
-#        client.disconnect()
-#    else:
-#        pass
-#        time.sleep(0.1)
-#    p1.wait()
-#    time.sleep(0.8)
