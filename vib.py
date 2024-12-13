ae8f64087e084fedcbce870157cd41ac3a4c1a31
node7-sh <sh@node7.org>
2024-12-13 22:43:14 +0100

    Update vib.py

--- vib.py
+++ vib.py
@@ -30,7 +30,7 @@ while True:
     next_start += intervall
     rest_zeit = next_start - time.time()
 
-    response = os.system("ping -c 1 " + "google.com  > /dev/null 2>&1")
+    response = subprocess.run(["ping", "-c1", „google.com“], capture_output=True, text=True)
     if response == 0:
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         client.connect(hostname,1883,60)
